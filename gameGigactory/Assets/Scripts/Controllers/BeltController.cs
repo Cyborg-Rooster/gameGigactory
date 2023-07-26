8fcc4661931c5136f37ca589ca1b55a457105bde
enrisco <64703041+enrisco@users.noreply.github.com>
2023-07-26 20:25:38 -0300

    v0.0.8.1
    - estava começando a incluir as esteiras mas minha mãe tava falando comigo e o casal maravilha chegou gritando

---
+++ gameGigactory/Assets/Scripts/Controllers/BeltController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class BeltController : MonoBehaviour
+{
+    [SerializeField] List<WorkbenchController> WorkbenchControllers;
+
+    public void InstantiateObjects(Belt belt)
+    {
+        //List<Workbench> workbenches = GameData.Workbenches.Where(x => GameData.Workbenches.Contains(x.BeltID))
+    }
+}
---
+++ gameGigactory/Assets/Scripts/Controllers/WorkbenchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class WorkbenchController : MonoBehaviour
+{
+    //[Header("Sprites")]
+    //[SerializeField] Sprite WorkbenchSprite;
+    //[SerializeField] Sprite BuyMore;
+
+    SpriteLevelController SpriteLevelController;
+
+    public void ChangeSpriteLevel(int level)
+    {
+        SpriteLevelController = GetComponent<SpriteLevelController>();
+        SpriteLevelController.ChangeSpriteLevel(level);
+    }
+}
